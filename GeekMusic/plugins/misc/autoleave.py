52aad4f83c0ae136b1bb1334d256f0a937c3a886
Geek Musik <106485862+GeekSuppurt@users.noreply.github.com>
2022-05-30 18:33:32 +0700

    Create autoleave.py

---
+++ GeekMusic/plugins/misc/autoleave.py
@@ -0,0 +1,84 @@
+#
+# Copyright (C) 2021-2022 by GeekMusik@Github, < https://github.com/GeekMusik >.
+#
+# This file is part of < https://github.com/GeekMusik/GeekMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/GeekMusik/GeekMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+from datetime import datetime
+
+import config
+from GeekMusic import app
+from GeekMusic.core.call import Geek, autoend
+from GeekMusic.utils.database import (get_client, is_active_chat,
+                                       is_autoend)
+
+
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
+            from GeekMusic.core.userbot import assistants
+
+            for num in assistants:
+                client = await get_client(num)
+                try:
+                    async for i in client.iter_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            "supergroup",
+                            "group",
+                            "channel",
+                        ]:
+                            chat_id = i.chat.id
+                            if (
+                                chat_id != config.LOG_GROUP_ID
+                                and chat_id != -1001443066504
+                                and chat_id != -1001602279044
+                                and chat_id != -1001741911479
+                            ):
+                                if not await is_active_chat(chat_id):
+                                    try:
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
+                                    except:
+                                        continue
+                except:
+                    pass
+
+
+asyncio.create_task(auto_leave())
+
+
+async def auto_end():
+    while not await asyncio.sleep(5):
+        if not await is_autoend():
+            continue
+        for chat_id in autoend:
+            timer = autoend.get(chat_id)
+            if not timer:
+                continue
+            if datetime.now() > timer:
+                if not await is_active_chat(chat_id):
+                    autoend[chat_id] = {}
+                    continue
+                autoend[chat_id] = {}
+                try:
+                    await Geek.stop_stream(chat_id)
+                except:
+                    continue
+                try:
+                    await app.send_message(
+                        chat_id,
+                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.",
+                    )
+                except:
+                    continue
+
+
+asyncio.create_task(auto_end())
