576f81f8e27807171ba7faa8c8eeb996edeaef1c
Geek Musik <106485862+GeekSuppurt@users.noreply.github.com>
2022-05-30 18:17:17 +0700

    Create __init__.py

---
+++ GeekMusic/platforms/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2021-2022 by GeekMusik@Github, < https://github.com/GeekMusik >.
+#
+# This file is part of < https://github.com/GeekMusik/GeekMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/GeekMusik/GeekMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
