faf1767eacd6749af6d998b876eab92a28dcfcb2
Geek Musik <106485862+GeekSuppurt@users.noreply.github.com>
2022-05-30 17:52:00 +0700

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021-2022 by GeekMusik@Github, < https://github.com/GeekMusik >.
+#
+# This file is part of < https://github.com/GeekMusik/GeekMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/GeekMusik/GeekMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
